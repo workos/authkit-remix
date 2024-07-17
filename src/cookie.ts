a7906a281ee0c804e6a45b06b179efc14d1eadee
Chance Strickland <chance.strickland@gmail.com>
2024-07-17 10:05:37 -0700

    Install prettier as dev dependency (#14)

--- src/cookie.ts
+++ src/cookie.ts
@@ -1,8 +1,4 @@
-import {
-  WORKOS_REDIRECT_URI,
-  WORKOS_COOKIE_MAX_AGE,
-  WORKOS_COOKIE_PASSWORD,
-} from './env-variables.js';
+import { WORKOS_REDIRECT_URI, WORKOS_COOKIE_MAX_AGE, WORKOS_COOKIE_PASSWORD } from './env-variables.js';
 import { createCookieSessionStorage } from '@remix-run/node';
 
 const redirectUrl = new URL(WORKOS_REDIRECT_URI);
@@ -17,17 +13,14 @@ const cookieOptions = {
   // Defaults to 400 days, the maximum allowed by Chrome
   // It's fine to have a long cookie expiry date as the access/refresh tokens
   // act as the actual time-limited aspects of the session.
-  maxAge: WORKOS_COOKIE_MAX_AGE
-    ? parseInt(WORKOS_COOKIE_MAX_AGE, 10)
-    : 60 * 60 * 24 * 400,
+  maxAge: WORKOS_COOKIE_MAX_AGE ? parseInt(WORKOS_COOKIE_MAX_AGE, 10) : 60 * 60 * 24 * 400,
   secrets: [WORKOS_COOKIE_PASSWORD],
 };
-const { getSession, commitSession, destroySession } =
-  createCookieSessionStorage({
-    cookie: {
-      name: cookieName,
-      ...cookieOptions,
-    },
-  });
+const { getSession, commitSession, destroySession } = createCookieSessionStorage({
+  cookie: {
+    name: cookieName,
+    ...cookieOptions,
+  },
+});
 
 export { cookieName, getSession, commitSession, destroySession };
