0acadde2e2ca564e6e2c682e1c9a06ccc0b70126
Nick Nisi <nick.nisi@workos.com>
2025-03-05 14:10:53 -0600

    add getWorkOS to the export, exposing it to end users

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { getSignInUrl, getSignUpUrl, signOut } from './auth.js';
 import { authLoader } from './authkit-callback-route.js';
 import { configure } from './config.js';
 import { authkitLoader } from './session.js';
+import { getWorkOS } from './workos.js';
 
 export {
   authLoader,
@@ -12,4 +13,5 @@ export {
   getSignUpUrl,
   signOut,
   configure,
+  getWorkOS,
 };
