a7b51acc8ec7873320eefafceb4c91d14c3be9b3
Kendall Swarthout <kendallstrautman@gmail.com>
2025-12-04 11:39:02 -0800

    bump version

--- src/workos.ts
+++ src/workos.ts
@@ -2,7 +2,7 @@ import { WorkOS } from '@workos-inc/node';
 import { getConfig } from './config.js';
 import { lazy } from './utils.js';
 
-const VERSION = '0.16.2';
+const VERSION = '0.17.0';
 
 /**
  * Create a WorkOS instance with the provided API key and optional settings.
